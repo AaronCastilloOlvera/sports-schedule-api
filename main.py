ea7b469f01f2e3f39b7784d2f83fc4e80dddbac9
Aarón Castillo <aaroncastilloolvera@gmail.com>
2025-06-01 18:37:09 -0600

    Add dotenv

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
-@app.get("/favicon.ico")
-def favicon():
-    return {}
