a03fcb78c59602b56c68d5e93b80ea5d5a2e75e6
Aarón Castillo <aaroncastilloolvera@gmail.com>
2026-01-04 00:53:29 -0600

    Update schemas.py

--- utils/schemas.py
+++ utils/schemas.py
@@ -14,6 +14,7 @@ class BettingTicketBase(BaseModel):
     status: str = "pending"  # 'pending', 'won', 'lost'
     match_name: str
     bet_type: Optional[str] = None
+    image_path: Optional[str] = None
     match_datetime: datetime
     device_type: str  # 'movil' or 'desktop'
     studied: bool = False
