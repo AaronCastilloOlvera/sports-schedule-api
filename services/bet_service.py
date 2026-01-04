148d56fc5373b1e470c373f1c9c6c77c12ef9281
Aarón Castillo <aaroncastilloolvera@gmail.com>
2026-01-04 11:48:47 -0600

    Update bet_service.py

--- services/bet_service.py
+++ services/bet_service.py
@@ -10,7 +10,7 @@ class BetService:
     self.folder = "tickets_images"
 
   def get_tickets(self):
-    return self.db.query(models.BettingTicket).all()
+    return self.db.query(models.BettingTicket).all().order_by(models.BettingTicket.ticket_id.desc())
   
   def get_ticket_by_id(self, ticket_id: str):
     return self.db.query(models.BettingTicket).filter(models.BettingTicket.ticket_id == ticket_id).first()
