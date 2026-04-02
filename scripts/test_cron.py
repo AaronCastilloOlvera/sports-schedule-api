50bec7ebc7976e53668c7588e71966a625486eb1
Aarón Castillo <57740438+AaronCastilloOlvera@users.noreply.github.com>
2026-04-01 23:23:42 -0600

    Create test_cron.py

---
+++ scripts/test_cron.py
@@ -0,0 +1,12 @@
+
+def test_worker():
+    print("Hello world")
+
+    for i in range(1, 11):
+        print(f"Iteration {i}")
+    
+    print("Worker finished")
+
+if __name__ == "__main__":
+    test_worker()
+
